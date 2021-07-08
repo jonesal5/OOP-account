de5f3c6ddabc40030d1d706680812bddc431dcfa
Alyssa <77090918+jonesal5@users.noreply.github.com>
2021-07-08 18:17:56 -0400

    Add files via upload

---
+++ bank_account.py
@@ -0,0 +1,38 @@
+class BankAccount:
+	def __init__(self, int_rate, balance): 
+        self.int_rate = 0.02
+        self.balance = 0
+	def deposit(self, amount):
+		self.account_balance += amount 
+    return self
+	def withdraw(self, amount):
+		self.account_balance -= amount
+	def display_account_info(self):
+		print(self.display_account_info)
+	def yield_interest(self):
+		self.yield_interest += amount
+
+#if there is not enough money, print a message "Insufficient funds: Charging a $5 fee"
+  def withdraw(self,amount):
+    if amount_after_withdrawal < 0:
+      print("insufficient funds: Charging a $5 fee")
+    else:
+      return self
+
+    #create two accounts
+    def __init__(self, name, email, account_balance):
+        self.name = name
+        self.email = email
+        self.int_rate = 0.02
+        self.account_balance = 0
+    
+    Alyssa = user("Alyssa K", "alyssak@codingdojo.com", "0")
+
+    Taytum = user("Taytum R", "taytumr@codingdojo.com", "0")
+
+    #make 3 deposits, 1 withdrawal, calculate interest, display account info
+    Alyssa.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(200).int_rate(0.02).display_account_info()
+
+    Taytum.make_deposit(700).make_deposit(200).make_withdrawal(100).make_withdrawal(100).make_withdrawal(100).int_rate(0.02).display_account_info()
+
+
